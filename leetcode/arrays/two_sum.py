0b276520a45ce3535a90688c543736824b04e700
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-25 15:22:36 -0300

    feat: Implement two sum with hash table

--- leetcode/arrays/two_sum.py
+++ leetcode/arrays/two_sum.py
@@ -9,3 +9,20 @@ def two_sum(nums: list[int], target: int) -> list[int]:
     return []
 
 
+def two_sum_hash_table(nums: list[int], target: int) -> list[int]:
+    """Hash table method"""
+    nums_length: int = len(nums)
+    hash_table: dict = {}
+    for i in range(nums_length):
+        complement = target - nums[i]
+        if complement in hash_table:
+            return [i, hash_table[complement]]
+        hash_table[nums[i]] = i
+    return []
+
+
+if __name__ == "__main__":
+    numbers: list[int] = [2, 7, 11, 15]
+    target: int = 9
+    result = two_sum_hash_table(numbers, target)
+    print(result)
